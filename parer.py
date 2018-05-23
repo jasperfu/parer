606aa8aee0d4c9929447d2394bda91cac54766c3
jfu <jasperfu@college.harvard.edu>
2018-05-23 12:07:43 -0700

    workingparer

--- parer.py
+++ parer.py
@@ -1,7 +1,7 @@
 import csv
-with open('batch63 raw.csv', newline='') as f:
+with open('dataload03262018.csv', newline='') as f:
     reader = csv.reader(f)
-    with open('batch63Pared.csv', 'w', newline='') as w:
+    with open('keeptrying.csv', 'w', newline='') as w:
         writer = csv.writer(w)
         writer.writerow(next(reader))
         batchId = 0
@@ -19,31 +19,36 @@ with open('batch63 raw.csv', newline='') as f:
         p12 = 0
         p13 = 0
         p14 = 0
-        morePrev = False
-        prevPoint = False
+        badcounter = 0
+        prevPoint = True
         keep = False
         gap = False
+        counter = 0
         for row in reader:
-            if p7 > max(p1, float(row[1]), p2, p3, p4, p5, p6,
-                        p8, p9, p10, p11, p12, p13, p14, 1)
-               and not morePrev:
-                morePrev = prevPoint
+            if (p7 > max(p1, float(row[1]), p2, p3, p4, p5, p6, p8, p9, p10, p11, p12, p13, p14, 1)) and not prevPoint:
                 prevPoint = keep
                 keep = True
-                print("up")
-            elif p7 < min(p1, float(row[1]), p2, p3, p4, p5, p6,
-                          p8, p9, p10, p11, p12, p13, p14, -1)
-                          and morePrev:
-                morePrev = prevPoint
+                print("up:")
+                print(p7)
+                if counter > 250:
+                    print("baaaaaaaaaaaaaaaaaaaaaaad")
+                    badcounter += 1
+                print("counter: " + str(counter))
+                counter = 0
+            elif (p7 < min(p1, float(row[1]), p2, p3, p4, p5, p6, p8, p9, p10, p11, p12, p13, p14, -1)) and prevPoint:
                 prevPoint = keep
                 keep = False
-                print("down")
+                print("down:")
+                print(p7)
+                print("counter: " + str(counter))
+                counter = 0
             if keep:
                 writer.writerow(next(reader) + [batchId])
                 gap = False
             elif gap == False:
                 batchId += 1
                 gap = True
+            counter += 1
 
             p14 = p13
             p13 = p12
